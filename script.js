446483479128a73e49e4256898a82698371c7700
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-30 17:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function insert_Row() {
     //Write your code here
-  let myTable = document.getElementById('sampleTable');
+  /*let myTable = document.getElementById('sampleTable');
 	
   let tr = document.createElement('tr');
 	for(let col=0;col<2;col++){
@@ -9,6 +9,15 @@ function insert_Row() {
 	  td.innerText = `New Cell${col+1}`;
 	  tr.appendChild(td);
 	}
-	myTable.prepend(tr);
+	myTable.prepend(tr);*/
+
+	var table = document.getElementById('sampleTable');
+	var newRow = table.insertRow(0);
+
+	var leftcell = newRow.insertCell(0);
+	var rightcell = newRow.insertCell(1);
+
+	leftcell.innerHTML = 'New Cell1';
+	rightcell.innerHTML = 'New Cell2';
 }
 
