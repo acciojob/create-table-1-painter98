4ee2de2c390ef494799e06d830bd6dd37d0d6b13
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-30 13:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+
+let row=3;
 function insert_Row() {
     //Write your code here
-  
-  
+  let myTable = document.getElementById('sampleTable');
+	
+  let tr = document.createElement('tr');
+	for(let col=0;col<2;col++){
+	  let td = document.createElement('td');
+	  td.innerText = `Row${row} cell${col+1}`;
+	  tr.appendChild(td);
+	}
+	myTable.appendChild(tr);
+	row++;
 }
+
